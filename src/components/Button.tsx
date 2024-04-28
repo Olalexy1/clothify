72fa76a85cb573a2c17e8223f05dee1a619c6d81
Olalekan Ajayi <91855086+Olalexy1@users.noreply.github.com>
2024-04-28 13:01:53 +0100

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+interface ButtonPops {
+    label: string;
+    iconURL?: string;
+    backgroundColor?: string;
+    textColor?: string;
+    borderColor?: string;
+    fullWidth?: boolean;
+}
+
+const Button: React.FC<ButtonPops> = ({
+    label,
+    iconURL,
+    backgroundColor,
+    textColor,
+    borderColor,
+    fullWidth,
+}) => {
+    return (
+        <button
+            className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+        ${backgroundColor
+                    ? `${backgroundColor} ${textColor} ${borderColor}`
+                    : "bg-coral-red text-white border-coral-red"
+                } rounded-full ${fullWidth && "w-full"}`}
+        >
+            {label}
+
+            {iconURL && (
+                <span className="inline-block transition-transform group-hover:translate-x-2">
+                    <Image
+                        src={iconURL}
+                        alt='arrow right icon'
+                        className='ml-2 rounded-full bg-white w-5 h-5'
+                    />
+                </span>
+            )}
+        </button>
+    );
+};
+
+export default Button;
---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ReviewCard from "./ReviewCard";
+import ServiceCard from "./ServiceCard";
+
+export {
+    Nav,
+    Button,
+    ShoeCard,
+    PopularProductCard,
+    ReviewCard,
+    ServiceCard
+}
